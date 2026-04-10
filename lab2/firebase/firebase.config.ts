957279cbbecfdb37c0ac8c5f9789a744d451534d
dmelgar1110 <a00839106@tec.mx>
2026-04-09 19:41:04 -0600

    Env para Lab 2

--- lab2/firebase/firebase.config.ts
+++ lab2/firebase/firebase.config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSz99l0beMXsdAGELbtKX6VwpURtzbQPo",
-  authDomain: "crud-firebase-5d417.firebaseapp.com",
-  projectId: "crud-firebase-5d417",
-  storageBucket: "crud-firebase-5d417.firebasestorage.app",
-  messagingSenderId: "150483310401",
-  appId: "1:150483310401:web:92d51a0eafe945ae4833c0"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
